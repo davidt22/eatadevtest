cc3e37addd00ca3cfd5f4bb23fa01c6d4938bfdb
David Teruel <d_teruel@hotmail.com>
2018-10-29 18:29:15 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+eatadevtest
+===========
+
+A Symfony project created on October 29, 2018, 5:17 pm.
---
+++ initialize-app-local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install vendors
+php -d memory_limit=-1 composer.phar install -o --no-interaction --prefer-dist
+
+
+# Install bower dependencies
+bower install --allow-root
+
+# Create database
+php bin/console doctrine:database:create
+
+# Apply schema changes
+php bin/console doctrine:schema:create
+
+# Load data fixtures to start the data in database
+php bin/console doctrine:fixtures:load -n
+
+# Install assets
+php bin/console assets:install web
+php bin/console assetic:dump
+
+# Change user and group of files
+chown -R www-data:www-data /var/www/
+
+# Grant permissions to cache and logs folders
+chmod -R 777 var/cache/ var/logs var/sessions
