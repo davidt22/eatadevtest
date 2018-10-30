e66d3db48ab3aace2a3fef3112824439c2653bd2
David Teruel <d_teruel@hotmail.com>
2018-10-30 11:46:29 +0100

    Added feature to list events

---
+++ src/AppBundle/Model/Event.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace AppBundle\Model;
+
+
+use JMS\Serializer\Annotation as Serializer;
+
+class Event
+{
+    /**
+     * @var int
+     * @Serializer\Type("int")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
+    private $date;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
